fbc37b2cf40883a55dda03f781e512121d048b2e
bryan kennedy <bkennedy@smm.org>
2013-01-25 17:18:09 -0600

    First version of the working addition functions
    The drag and drop will now do some basic addition. I've added a one,
    two, and a five glyph. This isn't interactive since it just leaves you
    with a full bucket after you drag everything in.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
-$(function() {
-  $( "div.glyphs" ).draggable();
-  $( "#bucket" ).droppable({
-    drop: function( event, ui ) {
-            $( this )
-    .addClass( "ui-state-highlight" )
-    .find( "p" )
-    .html( "Dropped!" );
-          }
-  });
+$(function () {
+    "use strict";
+    var $glyphs = $("#glyphs"),
+        $bucket = $("#bucket"),
+        number = 0;
+
+    function updateNumber(value, op) {
+        value = parseInt(value, 10);
+        var multiplier = 0;
+        switch (op) {
+        case 'addition':
+            multiplier = 1;
+            break;
+        case 'subtract':
+            multiplier = -1;
+            break;
+        }
+        number = number + (value * multiplier);
+        return number;
+    }
+
+    function displayNumber($item) {
+        var updadatedNumber = updateNumber($item.attr('data-glyph-value'), 'addition');
+        // Trying to set the internal HTML to the class of the dragged element
+        $("p", $bucket).html(updadatedNumber);
+    }
+
+    // Let the glyphs be draggable
+    $("img", $glyphs).draggable({
+        // Revert back to the original location if dropped outside the target
+        revert: "invalid"
+    });
+
+    // Make the bucket droppable
+    $("#bucket").droppable({
+        //accept: "div.glyphs",
+
+        drop: function (event, ui) {
+            displayNumber(ui.draggable);
+        }
+    });
+
 });
