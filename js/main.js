8f7cfd1898fc86d0b9b5b537b86ed9277e137a1b
Sarah German <sgerman@smm.org>
2013-02-19 15:54:34 -0600

    Adjusted code formatting and function order

--- js/main.js
+++ js/main.js
@@ -1,43 +1,40 @@
 $(function () {
-    "use strict";
-    var $glyphs = $("#glyphs"),
-        $bucket = $("#bucket"),
-        number = 0;
+  "use strict";
 
-    function updateNumber(value, op) {
-        value = parseInt(value, 10);
-        var multiplier = 0;
-        switch (op) {
-        case 'addition':
-            multiplier = 1;
-            break;
-        case 'subtract':
-            multiplier = -1;
-            break;
-        }
-        number = number + (value * multiplier);
-        return number;
+  var $glyphs = $('#glyphs'),
+      $bucket = $('#bucket'),
+      number = 0;
+
+  // Let the glyphs be draggable
+  $('img', $glyphs).draggable({
+    revert: 'invalid' // Revert back to the original location if dropped outside the target
+  });
+  // Make the bucket droppable
+  $('#bucket').droppable({
+    drop: function (event, ui) {
+      displayNumber(ui.draggable);
     }
+  });
 
-    function displayNumber($item) {
-        var updadatedNumber = updateNumber($item.attr('data-glyph-value'), 'addition');
-        // Trying to set the internal HTML to the class of the dragged element
-        $("p", $bucket).html(updadatedNumber);
+  function updateNumber(value, op) {
+    value = parseInt(value, 10);
+    var multiplier = 0;
+    switch (op) {
+    case 'addition':
+      multiplier = 1;
+      break;
+    case 'subtract':
+      multiplier = -1;
+      break;
     }
+    number = number + (value * multiplier);
+    return number;
+  }
 
-    // Let the glyphs be draggable
-    $("img", $glyphs).draggable({
-        // Revert back to the original location if dropped outside the target
-        revert: "invalid"
-    });
-
-    // Make the bucket droppable
-    $("#bucket").droppable({
-        //accept: "div.glyphs",
-
-        drop: function (event, ui) {
-            displayNumber(ui.draggable);
-        }
-    });
+  function displayNumber($item) {
+    var updadatedNumber = updateNumber($item.attr('data-glyph-value'), 'addition');
+    // Trying to set the internal HTML to the class of the dragged element
+    $('p', $bucket).html(updadatedNumber);
+  }
 
 });
