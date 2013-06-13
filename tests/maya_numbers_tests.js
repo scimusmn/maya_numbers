089ec8b066e81de2f6065400a8dce33d25404d3e
Sarah German <sgerman@smm.org>
2013-06-13 13:55:26 -0700

    Sketched out a few ideas for unit tests

---
+++ tests/maya_numbers_tests.js
@@ -0,0 +1,43 @@
+// @TODO - Set up qunit as a Git submodule
+
+/**
+ * Test: Do the language links change the language?
+ */
+test("language switcher", function() {
+
+});
+
+/**
+ * Test: Does the page refresh after 3min of inactivity?
+ */
+test("page refresh", function() {
+
+});
+
+/**
+ * Test: Does dropping a glyph in a bucket make the value appear?
+ */
+test("full bucket", function() {
+
+});
+
+/**
+ * Test: Do the values add up properly?
+ */
+test("glyph addition", function() {
+
+});
+
+/**
+ * Test: Does removing a glyph subtract properly?
+ */
+test("glyph subtraction", function() {
+
+});
+
+/**
+ * Test: Do you advance levels after getting the required problems right?
+ */
+test("level change", function() {
+
+});
