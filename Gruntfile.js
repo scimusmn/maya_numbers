dccd89549077328b5e26840c9aba23ffe426ef5a
Sarah German <sgerman@smm.org>
2013-07-10 17:16:44 -0500

    Same CSS/JS file load order for build and source

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,9 +8,9 @@ module.exports = function(grunt) {
           files: {
             'build/js/maya_numbers.min.js': [
               'libs/jquery-1.9.0.min.js',
-              'libs/jquery.hammer.min.js',
               'libs/jquery-ui-1.10.0.custom.min.js',
               'libs/jquery.ui.touch-punch.min.js',
+              'libs/jquery.hammer.min.js',
               'libs/flippant.js',
               'libs/tock.min.js',
               'source/js/main.js',
@@ -24,8 +24,8 @@ module.exports = function(grunt) {
       compress: {
         files: {
           'build/css/maya_numbers.min.css': [
-            'source/css/main.css',
             'source/css/normalize.min.css',
+            'source/css/main.css',
             'source/css/jquery-ui.min.css',
             'source/css/flippant.css',
             'source/css/build.css',
